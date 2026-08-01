974c2d2691c236001fdfe036b481587808808f84
Abdelrahman Sherif <sherif_jr@192.168.1.11>
2026-08-01 16:22:36 +0300

    first step of building my OS

---
+++ kernel.cpp
@@ -0,0 +1,12 @@
+void print(const char* str)
+{
+  unsigned short* VideoMemory = (unsigned short*) 0xb8000;
+  for(int i=0; str[i] != '\0'; i++)
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+extern "C" void kernel_main(void* multiboot_structure, unsigned int magic_number){
+  print("Hello, sherif ");
+
+  while(1);
+}
